1723d3c566b4211d517705a335d5b7f2da0c236e
hebale <hebale@naver.com>
2018-11-29 15:45:55 +0900

    181129_update
    real time chart modified

--- js/main.js
+++ js/main.js
@@ -1,41 +1,26 @@
 $(function(){
-	$("#realChart>li").on("mouseenter",function(){
-		$("#realChart>li").removeClass("on");
+	chartNum();		//실시간 차트 상승하강 아이콘 삽입값
+
+	moveChart();	//실시간 차트 스몰 차트 움직임
+	chartNumSm();	//실시간 차트 스몰 상승하강 아이콘 삽입값
+
+	$("#real_chart>li").on("mouseenter",function(){
+		$("#real_chart>li").removeClass("on");
 		$(this).addClass("on");
 	});
 
-	var chartUp = $(".rankSta.up").length;
-	var chartNone = $(".rankSta.none").length;
-	var chartDown = $(".rankSta.down").length;
-
-	for(var i =0; i < chartUp; i++){
-		var curUp = $(".rankSta.up").eq(i).html();
-		$(".rankSta.up").eq(i).html(curUp + "<i class='fas fa-caret-up'></i>");
-	}
-	for(var j =0; j < chartNone; j++){
-		var curNone = $(".rankSta.none").eq(j).html("-");
-	}
-	for(var k =0; k < chartDown; k++){
-		var curDown = $(".rankSta.down").eq(k).html();
-		$(".rankSta.down").eq(k).html(curDown + "<i class='fas fa-caret-down'></i>");
-	}
-
-
-
-
-
-
-
-
-	$("#new_song>ul").scroll(function(){
-		console.log($("#new_song>ul").scrollTop());
-
-		var shadowPos = ($("#new_song>ul").scrollTop() / 117.5) - 2;
+	$("#real_chart_sm>li").on("click",function(){
 		
-		$("#new_song>ul").css("box-shadow","-10px " + shadowPos + "px 15px -3px rgba(0,0,0,0.5) inset");
-
 	});
 
+	//-------------------------------------------
+	//-----------최신곡 그림자 스크립트------------
+	//-------------------------------------------
+	$("#new_song>ul").scroll(function(){
+		console.log($("#new_song>ul").scrollTop());
+		var shadowPos = ($("#new_song>ul").scrollTop() / 117.5) - 2;		
+		$("#new_song>ul").css("box-shadow","-10px " + shadowPos + "px 15px -3px rgba(0,0,0,0.5) inset");
+	});
 
 	//-------------------------------------------
 	//--------------슬라이드 스크립트--------------
@@ -62,3 +47,52 @@ $(function(){
 
 });
 
+
+
+//-------------------------------------------
+//------------실시간차트  스크립트-------------
+//-------------------------------------------
+
+function moveChart(){
+	var topPos = 0;
+	setInterval(function(){
+		topPos++;
+		if(topPos == 10) topPos = 0;
+		$("#real_chart_sm>ul").stop().animate({top:-35 * topPos},500);
+	},4000);
+}
+
+function chartNum(){
+	var chartUp = $(".rankSta.up").length;
+	var chartNone = $(".rankSta.none").length;
+	var chartDown = $(".rankSta.down").length;
+
+	for(var i =0; i < chartUp; i++){
+		var curUp = $(".rankSta.up").eq(i).html();
+		$(".rankSta.up").eq(i).html(curUp + "<i class='fas fa-caret-up'></i>");
+	}
+	for(var j =0; j < chartNone; j++){
+		var curNone = $(".rankSta.none").eq(j).html("-");
+	}
+	for(var k =0; k < chartDown; k++){
+		var curDown = $(".rankSta.down").eq(k).html();
+		$(".rankSta.down").eq(k).html(curDown + "<i class='fas fa-caret-down'></i>");
+	}
+}
+function chartNumSm(){
+	var chartUp = $(".rankSta_sm.up").length;
+	var chartNone = $(".rankSta_sm.none").length;
+	var chartDown = $(".rankSta_sm.down").length;
+
+	for(var i =0; i < chartUp; i++){
+		var curUp = $(".rankSta_sm.up").eq(i).html();
+		$(".rankSta_sm.up").eq(i).html(curUp + "<i class='fas fa-caret-up'></i>");
+	}
+	for(var j =0; j < chartNone; j++){
+		var curNone = $(".rankSta_sm.none").eq(j).html("-");
+	}
+	for(var k =0; k < chartDown; k++){
+		var curDown = $(".rankSta_sm.down").eq(k).html();
+		$(".rankSta_sm.down").eq(k).html(curDown + "<i class='fas fa-caret-down'></i>");
+	}
+}
